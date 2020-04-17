0825f5619a5896a92f4db9564d5d6f5b1a3f0364
Etienne Landuré <elandure@gmail.com>
2020-04-17 11:59:18 +0200

    Change how the request body is generated.

--- sphinxcontrib/openapi/openapi30.py
+++ sphinxcontrib/openapi/openapi30.py
@@ -286,16 +286,8 @@ def _httpresource(endpoint, method, properties, convert, render_examples,
         request_content = properties.get('requestBody', {}).get('content', {})
         if request_content and 'application/json' in request_content:
             schema = request_content['application/json']['schema']
-            req_properties = json.dumps(schema['properties'], indent=2,
-                                        separators=(',', ':'))
-            yield '{indent}**Request body:**'.format(**locals())
-            yield ''
-            yield '{indent}.. sourcecode:: json'.format(**locals())
-            yield ''
-            for line in req_properties.splitlines():
-                # yield indent + line
-                yield '{indent}{indent}{line}'.format(**locals())
-                # yield ''
+            for line in convert_json_schema(schema, directive=':<json'):
+                yield '{indent}{line}'.format(**locals())
 
     # print request example
     if render_examples:
